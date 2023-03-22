09eb889579877614aa647f2eff3185bfaa048b95
Emsthf <49848313+emsthf@users.noreply.github.com>
2023-03-22 17:20:29 +0900

    fix: optimistic lock을 이용해서 정합성 맞추기 장점 - 별도의 Lock을 잡지 않으므로 Pessimistic Lock보다 성능상 이점이 있다.
    단점
    - update가 실패했을 때 재시도 로직을 개발자가 직접 작성해줘야 함
    - 충돌이 빈번하게 일어날 것이라고 예상 된다면 Pessimistic Lock을 이용하는게 성능상 이점이 될 수 있다.

---
+++ src/main/java/com/example/stock/facade/OptimisticLockStockFacade.java
@@ -0,0 +1,26 @@
+package com.example.stock.facade;
+
+import com.example.stock.service.OptimisticLockStockService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OptimisticLockStockFacade {
+
+    private OptimisticLockStockService optimisticLockStockService;
+
+    public OptimisticLockStockFacade(OptimisticLockStockService optimisticLockStockService) {
+        this.optimisticLockStockService = optimisticLockStockService;
+    }
+
+    public void decrease(Long id, Long quantity) throws InterruptedException {
+        // 수량 감소에 실패하게 되면 50ms 뒤에 재시도
+        while (true) {
+            try {
+                optimisticLockStockService.decrease(id, quantity);
+                break;
+            } catch (Exception e) {
+                Thread.sleep(50);
+            }
+        }
+    }
+}
