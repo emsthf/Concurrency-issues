e56e2f80a27bd76ddab0585bfc7f93caa012596a
Emsthf <49848313+emsthf@users.noreply.github.com>
2023-03-22 14:37:12 +0900

    fix: 자바의 Synchronized를 사용해 동시성 이슈 해결 하지만 다수의 서버를 운용하게 되면 동일한 문제 발생

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -39,4 +43,31 @@ class StockServiceTest {
 
         assertEquals(99, stock.getQuantity());
     }
+
+    @Test
+    public void concurrency_issue() throws InterruptedException {
+        // 동시에 100개의 요청
+        int threadCount = 100;
+        // ExecutorService는 비동기로 실행하는 작업을 단순화하여 사용할 수 있게 도와주는 자바의 API
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        // 100번의 요청이 끝날 때까지 기다려야 하므로 CountDownLatch 사용
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - (1 * 100) = 0
+        assertEquals(0L, stock.getQuantity());
+    }
+
 }
